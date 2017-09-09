376c16174df5c66f668eb34715a8cd2551270e17
RC <RelentlessCoffee@gmail.com>
2017-09-08 23:57:20 -0700

    added product url and vendor

--- data.py
+++ data.py
@@ -7,18 +7,20 @@ class Product(BaseModel):
     name = Column(String, hash_key=True, name="n")
     date = Column(DateTime, range_key=True, name="d")
     quantity = Column(Integer, name="q")
+    url = Column(String, name="u")
 
 
 engine = Engine()
 engine.bind(Product)
 
 
-def insert_row(name: str, variation: str, quantity: int, date: pendulum.Pendulum):
+def insert_row(vendor: str, name: str, variation: str, quantity: int, date: pendulum.Pendulum, url: str):
     product = Product()
-    product.name = name + "." + variation
+    product.name = vendor + "." + name + "." + variation
     product.date = date
     product.quantity = quantity
+    product.url = url
     engine.save(product)
 
 
-# insert_row("repave", "25g", 2041, pendulum.now())
+# insert_row("w2t", "repave", "25g", 2041, pendulum.now(), "my-url")
