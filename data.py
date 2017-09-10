d442e25b755bf4ebc75f97ee79a09e23969943bd
RC <RelentlessCoffee@gmail.com>
2017-09-10 16:43:33 -0700

    changed product key structure, added original name column

--- data.py
+++ data.py
@@ -1,25 +1,36 @@
+from typing import Optional
 from bloop import BaseModel, Column, String, Integer, Engine
 from bloop.ext.pendulum import DateTime
 import pendulum
 
 
 class Product(BaseModel):
+    class Meta:
+        write_units = 5
     name = Column(String, hash_key=True, name="n")
     date = Column(DateTime, range_key=True, name="d")
     quantity = Column(Integer, name="q")
     url = Column(String, name="u")
+    og_name = Column(String, name="og")
 
 
 engine = Engine()
 engine.bind(Product)
 
 
-def insert_row(vendor: str, name: str, variation: str, quantity: int, date: pendulum.Pendulum, url: str):
+def make_product_name(vendor: str, name: str, weight: str):
+    name = name.lower().replace(" ", "-").replace(".", "_")
+    product_name = vendor + "." + name + "." + weight
+    return product_name
+
+
+def insert_row(vendor: str, name: str, weight: str, quantity: Optional[int], date: pendulum.Pendulum, url: str):
     product = Product()
-    product.name = vendor + "." + name + "." + variation
+    product.name = make_product_name(vendor, name, weight)
     product.date = date
     product.quantity = quantity
     product.url = url
+    product.og_name = name
     engine.save(product)
 
 
