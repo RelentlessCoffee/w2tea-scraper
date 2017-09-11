3bb105c795b649f05b88ba58e5b0cd31201d9e01
RC <RelentlessCoffee@gmail.com>
2017-09-10 18:25:45 -0700

    created data model for tracking imports

--- data.py
+++ data.py
@@ -14,14 +14,31 @@ class Product(BaseModel):
     og_name = Column(String, name="og")
 
 
+class VendorImports(BaseModel):
+    vendor = Column(String, hash_key=True, name="v")
+    last_run_at = Column(DateTime, name="lr")
+
+
 engine = Engine()
 engine.bind(Product)
+engine.bind(VendorImports)
 
 
 def make_product_name(vendor: str, name: str, weight: str):
     name = name.lower().replace(" ", "-").replace(".", "_")
     product_name = vendor + "." + name + "." + weight
     return product_name
+
+
+def get_last_import_date(vendor: str):
+    run = VendorImports(vendor=vendor)
+    engine.load(run)
+    return run.last_run_at
+
+
+def set_last_import_date(vendor: str, date: pendulum.Pendulum):
+    run = VendorImports(vendor=vendor, last_run_at=date)
+    engine.save(run)
 
 
 def insert_row(vendor: str, name: str, weight: str, quantity: Optional[int], date: pendulum.Pendulum, url: str):
