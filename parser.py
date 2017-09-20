d8fdbf707b756c0bcc89bbacab090f519e711754
RC <RelentlessCoffee@gmail.com>
2017-09-20 00:20:37 -0700

    rewrote form and page parsing

--- parser.py
+++ parser.py
@@ -1,7 +1,7 @@
 import boto3
 import base64
-import pendulum
 import bs4
+import pendulum
 import json
 from s3 import Cache
 from data import insert_row, get_last_import_date, set_last_import_date
@@ -24,7 +24,7 @@ def list_objects(vendor: str, uploaded_on: pendulum.Pendulum):
         decoded_url = base64.b64decode(encoded_url)
         decoded_url = decoded_url.decode("utf-8")
         obj = {
-            "vendor": vendor,
+            "vendor_id": vendor,
             "date": date,
             "url": decoded_url,
             "item": item
@@ -51,73 +51,109 @@ def list_days_between(start: pendulum.Pendulum, end: pendulum.Pendulum):
 
 def parse_and_upload(obj):
     soup = get_html(obj)
-    name = find_name(soup)
-    options = find_options(soup)
-    date = obj["date"]
-    vendor = obj["vendor"]
-    url = obj["url"]
-    for option in options:
-        weight = option["weight"]
-        quantity = option["quantity"]
-        insert_row(vendor, name, weight, quantity, date, url)
+    parsed_data = form_try(soup, obj["vendor_id"], obj["url"], obj["date"])
+    for entry in parsed_data:
+        insert_row(**entry)
 
 
-def parse_and_upload_vendor(vendor: str):
-    start = get_last_import_date(vendor)
+def parse_and_upload_vendor(vendor_id: str):
+    start = get_last_import_date(vendor_id)
     end = pendulum.utcnow()
     days = list_days_between(start, end)
     for date in days:
-        objs = list_objects(vendor, date)
+        objs = list_objects(vendor_id, date)
         for obj in objs:
             parse_and_upload(obj)
-        set_last_import_date(vendor, date)
+        set_last_import_date(vendor_id, date)
 
 
-class NoMaxQuantity(Exception):
-    pass
+def form_try(soup, vendor_id, url, date):
+    data = parse_from_form(soup)
+    if data is None:
+        data = [parse_from_page(soup)]
+    for entry in data:
+        entry["name"] = find_name(soup)
+        entry["vendor_id"] = vendor_id
+        entry["url"] = url
+        entry["date"] = date
+    return data
 
 
-def _find_options_from_form(soup):
+def parse_from_form(soup):
     form = soup.find("form", class_="variations_form cart")
     if form is None:
-        raise NoMaxQuantity("no max quantity")
+        return None
     product_data = form['data-product_variations']
-    product_data = json.loads(product_data)
-    quantities = [option["max_qty"] for option in product_data]
-    options = soup.find_all("option")[1:]
-    options = [option["value"] for option in options]
-    # return list(zip(options, quantities))
-    option_dicts = []
-    for option, quantity in zip(options, quantities):
-        try:
+    form = json.loads(product_data)
+    parsed_data = []
+    for entry in form:
+        quantity = entry["max_qty"]
+        if quantity is not None:
             quantity = int(quantity)
-        except (TypeError, ValueError):
-            quantity = None
-        option_dicts.append({
-            "weight": option,
-            "quantity": quantity
-            })
-    return option_dicts
+        parsed_data.append({
+            "sku": entry['sku'],
+            "variation": str(entry['variation_id']),
+            "weight": entry['attributes']['attribute_pa_amount'],
+            "quantity": quantity,
+            "price": float(entry['display_price'])
+        })
+    return parsed_data
 
 
-def _find_options_from_input(soup):
+def find_price(soup):
+    el = soup.find("p", class_="price")
+    if el is None:
+        return None
+    prices = el.find_all("span", class_="woocommerce-Price-amount amount")
+    assert len(prices) == 1, prices
+    price = prices[0].contents[-1]
+    return float(price)
+
+
+def find_quantity_page(soup):
     product_data = soup.find("input", class_="input-text qty text")
     if product_data is None:
-        return [{"weight": "", "quantity": None}]
+        return None
     try:
         quantity = product_data['max']
     except KeyError:
         quantity = ""
     if quantity == "":
-        return [{"weight": "", "quantity": None}]
-    return [{"weight": "", "quantity": int(quantity)}]
+        return None
+    return int(quantity)
 
 
-def find_options(soup):
+def find_variation_page(soup):
+    variation = soup.find("input", attrs={"name": "add-to-cart"})
+    if variation is None:
+        return ""
     try:
-        return _find_options_from_form(soup)
-    except NoMaxQuantity:
-        return _find_options_from_input(soup)
+        return str(variation["value"])
+    except KeyError:
+        return ""
+
+
+def find_sku_page(soup):
+    el = soup.find("span", class_="sku")
+    if el is None:
+        return ""
+    try:
+        sku = el.text
+    except KeyError:
+        return ""
+    return sku
+
+
+def parse_from_page(soup):
+    parsed_data = {
+        "sku": find_sku_page(soup),
+        "variation": find_variation_page(soup),
+        "quantity": find_quantity_page(soup),
+        # there is no weight when there is no form
+        "weight": "",
+        "price": find_price(soup)
+    }
+    return parsed_data
 
 
 def find_name(soup):
@@ -126,5 +162,5 @@ def find_name(soup):
 
 
 if __name__ == '__main__':
-    vendor = "w2t"
-    parse_and_upload_vendor(vendor)
+    vendor_id = "w2t"
+    parse_and_upload_vendor(vendor_id)
